96ca2d23270b1424c6118b542256de8351306e50
Marie-Lyse Charriere <marielysech@gmail.com>
2022-04-06 17:05:40 +0200

    database logic added for GET/PUT/DELETE request

--- api.js
+++ api.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const path = require('path');
 app.use(express.json())
 
 const port = 3000
@@ -14,7 +13,7 @@ app.listen(port, hostName, (err) => {
 
 //Database
 
-let taskDatabase = []; //will include all typicalEntrie
+let taskDatabase = []; //will include all typicalEntries
 
 // let typicalEntry = {
 //    id: //number for each new entry it will be = array.length
@@ -33,45 +32,58 @@ let taskDatabase = []; //will include all typicalEntrie
 // '/tasks/' endpoint
 app.route('/tasks')
     .get((req, res) => {
-        res.send(__dirname + `/index.html`);
-        // res.send('here are all the tasks');
+        res.send(taskDatabase);
     }) //access all tasks
 
     .post((req, res) => {
-        res.send(`create the task ${req.body.name}`);
+        function idAssignation() {
+            if (taskDatabase.length < 1) {
+                return 1
+            } else if (taskDatabase[taskDatabase.length + 1]) {
+                return 'error id already taken'
+            } else
+            return taskDatabase.length + 1
+        }
+
+        let newEntry = {
+            action: req.body.action,
+            status: "todo"
+        }
+        newEntry.id = idAssignation()
+        taskDatabase.push(newEntry)
+        res.redirect('/task');
     }); //create new task
 
 
 // '/tasks/:id' endpoint
 app.route('/tasks/:id')
     .get((req, res) => {
-        res.send(`here is the ${req.params.id} task`);
+        res.send(taskDatabase.filter(element => element.id === req.params.id));
     }) //access specific tasks
 
     .put((req, res) => {
-    res.send(`update the   ${req.params.id} task`);
+        let index = taskDatabase.findIndex( element => element.id === req.params.id)
+        taskDatabase[index].status = req.body.status
+        taskDatabase[index].action = req.body.action
+        res.send(`Task ${req.params.id} has been updated`);
 
     }) //update specific tasks
 
     .delete((req, res) => {
-    res.send(`delete the ${req.params.id} task`);
+        let index = taskDatabase.findIndex( element => element.id === req.params.id)
+        taskDatabase.splice(index, 1)
+        
+        res.send(`Task ${req.params.id} has been deleted`);
     }); //supress specific tasks
-
-
-// /tasks/:id/:status
-app.put('/tasks/:id/:status', (req, res) => {
-    res.send(`modify your ${req.params.id} task status`);
-
-}); // modify status from specific task
-
-// /tasks/:id/:tag
-app.put('/tasks/:id/:tag', (req, res) => {
-    res.send('modify your task tag');
-
-}); //update tag of a specific task
-
 
 // /tasks/:status
 app.delete('/tasks/:status', (req, res) => {
-    res.send('modify your task status');
+    taskDatabase.forEach(function(elem) {
+        if(elem.status === req.params.status) {
+            taskDatabase.splice(taskDatabase.indexOf(elem), 1);
+        }
+    })    
+
+    res.send(`All ${req.params.status} have been removed from the list`);
 }); // remove all tasks from specific status
+
