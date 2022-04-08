256279ba96010a944e0b332b8e1fafbb2034f973
Marie-Lyse Charriere <marielysech@gmail.com>
2022-04-08 11:43:34 +0200

    refactoring code under MVC model

---
+++ controllers/taskControllers.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const ejs = require('ejs');
+
+const taskDatabaseJSON = fs.readFileSync('public/storage.json')
+const taskJson = JSON.parse(taskDatabaseJSON) // convert in JSON format
+
+function getAllTasks(req, res) { //access all tasks
+     if (taskJson) {
+        console.log(taskJson)
+        return res.send(taskJson)
+    } return res.send("No task yet")
+    // res.render('index', { tasksArray: taskDatabase });
+    }
+
+function createTask(req, res) { 
+    let newEntry = {
+        id: Date.now(),
+        action: req.body.action,
+        status: "todo"
+    }
+
+    taskJson.push(newEntry);
+    const newData = JSON.stringify(taskJson) //back to row format
+    fs.writeFile("public/storage.json", newData, (err) => {
+        if (err) throw err;
+            console.log("  New data added");
+        });
+    res.status(200)  
+    res.send(`${newEntry.action} added`);
+}
+
+
+function getTask(req, res) {
+       const task = taskJson.filter(element => element.id === parseInt(req.params.id));
+    if (task) {
+        console.log(task)
+        return res.send(task);
+    } return res.send(`The task with id:${req.params.id} doesn't exist`)
+     } 
+
+function updateTask(req, res) { 
+        let index = taskJson.findIndex( element => element.id === parseInt(req.params.id))
+        taskJson[index].status = req.body.status || taskJson[index].status
+        taskJson[index].action = req.body.action || taskJson[index].action
+        res.send(`Task ${req.params.id} has been updated`);
+
+    }
+
+function deleteTask(req, res) { 
+        let index = taskJson.findIndex( element => element.id === parseInt(req.params.id))
+        if (index) {
+            taskJson.splice(index, 1)
+            return res.send(`Task ${req.params.id} has been deleted`);
+        } return res.send("This task doesn't exist")
+    }
+
+
+// not working nor used for now    
+function deleteByStatus(req, res) { 
+    let filterArray = taskDatabase.filter((element) => {toString(element.status) !== toString(req.params.status)})
+    taskDatabase = filterArray
+
+    // taskDatabase.forEach(function(elem) {
+    //     if(elem.status === req.query.status) {
+    //         taskDatabase.splice(taskDatabase.indexOf(elem), 1);
+    //     }
+    // })    
+
+    return res.send(`All ${req.params.status} have been removed from the list`);
+}
+
+module.exports = {getAllTasks, createTask, getTask, updateTask, deleteTask, taskJson, taskDatabaseJSON}
---
+++ routes/taskRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router()
+const taskController = require('../controllers/taskControllers')
+
+
+router.route('/')
+    .get(taskController.getAllTasks)
+    .post(taskController.createTask)
+
+
+router.route('/:id')
+    .get(taskController.getTask) 
+    .put(taskController.updateTask) 
+    .delete(taskController.deleteTask);
+
+module.exports = router;
